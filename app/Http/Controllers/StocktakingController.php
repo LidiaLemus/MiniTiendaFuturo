a69e778649cafb14b20e23da79e83b13edc88d2b
LidiaLemus <lidiaagustinesq1997@gmail.com>
2019-10-17 09:25:00 -0600

    Controlador stocktaking added

---
+++ app/Http/Controllers/StocktakingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Stocktaking;
+use Illuminate\Http\Request;
+
+class StocktakingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Stocktaking  $stocktaking
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Stocktaking $stocktaking)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Stocktaking  $stocktaking
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Stocktaking $stocktaking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Stocktaking  $stocktaking
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Stocktaking $stocktaking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Stocktaking  $stocktaking
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Stocktaking $stocktaking)
+    {
+        //
+    }
+}
