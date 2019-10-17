d8b4e2cf7f95e3a8d3563c54ef02562cf74d3486
LidiaLemus <lidiaagustinesq1997@gmail.com>
2019-10-17 15:15:17 -0600

    added date of table costumer

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    protected $table = 'customers';
+    protected $fillable = ['fullname','email','address','phone','nit'];
+
+   
 }
