7498a00e6cdd9262e087ccc0c4050d48cd28e170
LidiaLemus <lidiaagustinesq1997@gmail.com>
2019-10-17 09:23:52 -0600

    Controlador PurchaseDetail added

---
+++ app/Http/Controllers/PurchaseDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Purchase_Detail;
+use Illuminate\Http\Request;
+
+class PurchaseDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Purchase_Detail  $purchase_Detail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Purchase_Detail $purchase_Detail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Purchase_Detail  $purchase_Detail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Purchase_Detail $purchase_Detail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Purchase_Detail  $purchase_Detail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Purchase_Detail $purchase_Detail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Purchase_Detail  $purchase_Detail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Purchase_Detail $purchase_Detail)
+    {
+        //
+    }
+}
