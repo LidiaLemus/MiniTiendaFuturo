b4d264c8150c0be2450b62d2d297609855dafba2
LidiaLemus <lidiaagustinesq1997@gmail.com>
2019-10-18 16:27:51 -0600

    New change in the model Product_has_Sale

--- app/Product_has_Sale.php
+++ app/Product_has_Sale.php
@@ -8,6 +8,6 @@ class Product_has_Sale extends Model
 {
     protected $table = 'product_has_sales';
     protected $fillable = [
-        'price','quantity','subtotal', 'sale_id', 'product_id','invoice_id'
+        'price','quantity','subtotal', 'sale_id', 'product_id'
     ];
 }
