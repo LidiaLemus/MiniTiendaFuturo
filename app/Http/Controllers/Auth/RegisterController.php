8951828b1dc50733ec20ed5920fc3b71bbda3e02
LidiaLemus <lidiaagustinesq1997@gmail.com>
2020-01-28 20:12:41 -0600

    Password reset

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,7 +31,7 @@ class RegisterController extends Controller
     protected $redirectTo = '/';
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('guest');
     }
 
     /**
